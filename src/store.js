6a24689ff5b457f48a1eabb8ec8e23a7e179c3bf
JoeVincentt <eugene.bb@hotmail.com>
2018-11-26 00:38:45 -0500

    Invalid credentials Alert

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import "firebase/firestore";
 import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
 import { reduxFirestore, firestoreReducer } from "redux-firestore";
 //Reducers
-//@TODO
+import notifyReducer from "./reducers/notifyReducer";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCAhS9mQWrONcJoyPSBnNi-FD2LXG76gnM",
@@ -39,7 +39,8 @@ const createStoreWithFirebase = compose(
 // Add firebase to reducers
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer // <- needed if using firestore
+  firestore: firestoreReducer, // <- needed if using firestore
+  notify: notifyReducer
 });
 
 //Create initial state
