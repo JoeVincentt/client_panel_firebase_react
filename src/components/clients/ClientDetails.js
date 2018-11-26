7b5c249f3aeecf944e3a82074ecb39ce85d1953b
JoeVincentt <eugene.bb@hotmail.com>
2018-11-25 20:59:48 -0500

    delete added

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -5,11 +5,71 @@ import { compose } from "redux";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import Spinner from "../layout/Spinner";
-import Clients from "./Clients";
+import classnames from "classnames";
 
 class ClientDetails extends Component {
+  state = {
+    showBalanceUpdate: false,
+    balanceUpdateAmount: ""
+  };
+
+  //Update Balance
+  balanceSubmit = e => {
+    e.preventDefault();
+
+    const { client, firestore } = this.props;
+    const { balanceUpdateAmount } = this.state;
+
+    const clientUpdate = {
+      balance: parseFloat(balanceUpdateAmount)
+    };
+
+    //Update in firestore
+    firestore.update({ collection: "clients", doc: client.id }, clientUpdate);
+  };
+
+  //Dlete client
+  onDeleteClick = () => {
+    const { client, firestore, history } = this.props;
+
+    firestore
+      .delete({ collection: "clients", doc: client.id })
+      .then(history.push("/"));
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
     const { client } = this.props;
+    const { showBalanceUpdate, balanceUpdateAmount } = this.state;
+
+    let balanceForm = "";
+    //if balance form should display
+    if (showBalanceUpdate) {
+      balanceForm = (
+        <form onSubmit={this.balanceSubmit}>
+          <div className="input-group">
+            <input
+              type="number"
+              className="from-control"
+              name="balanceUpdateAmount"
+              placeholder="Add New Balance"
+              value={balanceUpdateAmount}
+              onChange={this.onChange}
+            />
+            <div className="input-group-append">
+              <input
+                type="submit"
+                value="Update"
+                className="btn btn-outline-dark"
+              />
+            </div>
+          </div>
+        </form>
+      );
+    } else {
+      balanceForm = null;
+    }
 
     if (client) {
       return (
@@ -25,7 +85,9 @@ class ClientDetails extends Component {
                 <Link to={`/client/edit/${client.id}`} className="btn btn-dark">
                   Edit
                 </Link>
-                <button className="btn btn-danger">Delete</button>
+                <button onClick={this.onDeleteClick} className="btn btn-danger">
+                  Delete
+                </button>
               </div>
             </div>
           </div>
@@ -44,10 +106,44 @@ class ClientDetails extends Component {
                 </div>
                 <div className="col-md-4 col-sm-6">
                   <h3 className="pull-right">
-                    Balance: ${parseFloat(client.balance).toFixed(2)}
+                    Balance:{" "}
+                    <span
+                      className={classnames({
+                        "text-danger": client.balance > 0,
+                        "text-success": client.balance <= 0
+                      })}
+                    >
+                      ${parseFloat(client.balance).toFixed(2)}
+                    </span>
+                    <small>
+                      <a
+                        href="#!"
+                        onClick={() =>
+                          this.setState({
+                            showBalanceUpdate: !this.state.showBalanceUpdate
+                          })
+                        }
+                      >
+                        <i
+                          className="fas fa-pencil-alt ml-2"
+                          style={{ fontSize: "18px" }}
+                        />
+                      </a>
+                    </small>
                   </h3>
+                  {balanceForm}
                 </div>
               </div>
+
+              <hr />
+              <ul className="list-group">
+                <li className="list-group-item">
+                  Contact Email: {client.email}
+                </li>
+                <li className="list-group-item">
+                  Contact Phone: {client.phone}
+                </li>
+              </ul>
             </div>
           </div>
         </div>
@@ -57,6 +153,10 @@ class ClientDetails extends Component {
     }
   }
 }
+
+ClientDetails.propTypes = {
+  firestore: PropTypes.object.isRequired
+};
 
 export default compose(
   firestoreConnect(props => [
